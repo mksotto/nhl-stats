3b33ed964c74939304d0576cd17adc7ab2f1558f
mksotto <maks.otto06@mail.ru>
2025-03-18 18:56:38 +0300

    Old types related on search and spotlight were removed

--- src/api/api-web.nhle/playerSpotlightGet.ts
+++
@@ -1,7 +0,0 @@
-import { PlayerSpotlightGet } from "../../types/playerSpotlightGet";
-import { makeRequest } from "./base";
-
-export const playerSpotlightGet = () => makeRequest<PlayerSpotlightGet[]>({
-    url: '/player-spotlight',
-    method: 'GET',
-})
